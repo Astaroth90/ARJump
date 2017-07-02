0c8a93dda9f159ab9a88943c83f83b785a113da8
Philipp Sossong <Psossong@gmx.de>
2017-07-02 13:45:21 +0200

    Add GoalFlag.

---
+++ ARJump/Assets/Goal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+
+    [SerializeField]float GoalDistance = 0.1f;
+    private bool isPlaced = false;
+    private GameObject mario;
+    private float distance;
+	// Use this for initialization
+	void Start () {
+        GetComponent<MeshRenderer>().enabled = false;
+        mario = GameObject.FindGameObjectWithTag("Mario");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isPlaced)
+        {
+            distance = Vector3.Distance(this.transform.position, mario.transform.position);
+            if(distance < GoalDistance)
+            {
+
+            }
+        }
+	}
+
+    public void placeGoal()
+    {
+
+    }
+}
